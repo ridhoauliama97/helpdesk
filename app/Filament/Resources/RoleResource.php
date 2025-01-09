0ac946b3dfcba90fbd57d9c725244c87260eb9e6
ridhoauliama97 <ridhoauliama97@gmail.com>
2025-01-09 14:58:33 +0700

    first commit

---
+++ app/Filament/Resources/RoleResource.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\RoleResource\Pages;
+use App\Filament\Resources\RoleResource\RelationManagers;
+use App\Models\Permission;
+use App\Models\Role;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+
+class RoleResource extends Resource
+{
+    protected static ?string $model = Role::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-lock-open';
+
+    protected static ?int $navigationSort = 3;
+
+    public static function getNavigationBadge(): ?string
+    {
+        return static::getModel()::count();
+    }
+
+    protected static function getNavigationLabel(): string
+    {
+        return __('Roles');
+    }
+
+    public static function getPluralLabel(): ?string
+    {
+        return static::getNavigationLabel();
+    }
+
+    protected static function getNavigationGroup(): ?string
+    {
+        return __('Permissions');
+    }
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Card::make()
+                    ->schema([
+                        Forms\Components\Grid::make()
+                            ->columns(1)
+                            ->schema([
+                                Forms\Components\TextInput::make('name')
+                                    ->label(__('Permission name'))
+                                    ->unique(table: Permission::class, column: 'name')
+                                    ->maxLength(255)
+                                    ->required(),
+
+                                Forms\Components\CheckboxList::make('permissions')
+                                    ->label(__('Permissions'))
+                                    ->required()
+                                    ->columns(4)
+                                    ->relationship('permissions', 'name'),
+                            ]),
+                    ])
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->label(__('Permission name'))
+                    ->sortable()
+                    ->searchable(),
+
+                Tables\Columns\TagsColumn::make('permissions.name')
+                    ->label(__('Permissions'))
+                    ->limit(2),
+
+                Tables\Columns\TextColumn::make('created_at')
+                    ->label(__('Created at'))
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true)
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->label(__('Last Update'))
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true)
+                    ->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListRoles::route('/'),
+            'create' => Pages\CreateRole::route('/create'),
+            'view' => Pages\ViewRole::route('/{record}'),
+            'edit' => Pages\EditRole::route('/{record}/edit'),
+        ];
+    }
+}
